8b3b695f24bceea8de8c9c51987eb6904c175858
CHAKKIT <chakkit.sa@hotmail.com>
2021-07-08 10:36:39 +0700

    disable debug

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -46,6 +46,7 @@ class LoginController extends Controller
      */
     public function login(Request $request)
     {   
+        //dd($request->all);
         $input = $request->all();
   
         $this->validate($request, [
