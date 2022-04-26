ecfee46537290d59e9c5bcb2afda2e4f63c3fcc1
CHAKKIT <chakkit.sa@hotmail.com>
2022-04-26 17:31:26 +0700

    t

--- resources/views/front-end/master.blade.php
+++ resources/views/front-end/master.blade.php
@@ -3,7 +3,7 @@
 
 <head>
   <meta charset="utf-8">
-  <title>Doi Chang Enduro Challenge</title>
+  <title>Doi Chang Enduro 2 Challenge</title>
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
   <meta content="" name="keywords">
   <meta content="" name="description">
